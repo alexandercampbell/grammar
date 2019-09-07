5c8aa7fb49b50c158120edcf628aa1862751725e
Alexander Campbell <alexanderhcampbell@gmail.com>
2019-09-06 23:56:22 -0500

    Implement basic pattern-matching

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -1,10 +1,23 @@
 module Grammar where
 
-data Terminal = String
-
 data Rule
-    = RuleTerminal Terminal
-    | RuleDisjunction [Rule]
+    = Literal String
+    | OneOrMore Rule
+    | Disjunction [Rule]
+    | Sequence [Rule]
+    deriving (Eq, Show)
+
+data Grammar = Grammar
+    { start :: Rule
+    }
+    deriving (Eq, Show)
+
+matchRule :: Rule -> String -> Bool
+matchRule (Literal     l   ) s = l == s
+matchRule (Disjunction list) s = any id $ map (\r -> matchRule r s) list
+
+matches :: Grammar -> String -> Bool
+matches grammar str = False
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,20 +6,36 @@ verbose = True
 
 assert :: String -> Bool -> IO ()
 assert msg cond = if cond
-  then when verbose $ putStrLn $ "Success: " ++ msg
-  else putStrLn $ "FAIL: " ++ msg
+    then when verbose $ putStrLn $ "Success: " ++ msg
+    else putStrLn $ "FAIL: " ++ msg
 
 assertEq :: (Eq a, Show a) => String -> a -> a -> IO ()
 assertEq msg expected actual = if expected == actual
-  then
-    when verbose
-    $  putStrLn
-    $  "Success: expected "
-    ++ show expected
-    ++ ", got it"
-  else
-    putStrLn $ "FAIL: expected " ++ show expected ++ ", actual " ++ show actual
+    then
+        when verbose
+        $  putStrLn
+        $  "Success: expected "
+        ++ show expected
+        ++ ", got it"
+    else
+        putStrLn
+        $  "FAIL: expected "
+        ++ show expected
+        ++ ", actual "
+        ++ show actual
 
+arithmeticGrammar =
+    let number = OneOrMore $ Disjunction $ map (Literal . show) [0 .. 9]
+        plus   = Sequence [number, Literal "+", expr]
+        minus  = Sequence [number, Literal "-", expr]
+        expr   = Disjunction [plus, minus, number]
+    in  Grammar { start = expr }
 
 main :: IO ()
-main = assertEq "Test" True True
+main = do
+    assert "literal rule matches literal text" $ matchRule (Literal "1") "1"
+    assert "disjunction matches one of the options" $ matchRule
+        (Disjunction [Literal "1", Literal "2", Literal "3"])
+        "2"
+    assert "arithmeticGrammar matches basic string"
+        $ matches arithmeticGrammar "1 + 2 - 3"
